0cecf662879a9d23e405500b5a30c3ab20ae2e80
NicolasGMACK <nicolasgmack@gmail.com>
2025-07-17 09:48:19 -0300

    fix: aparece acertos e erros, muda para próxima questão

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
 const questoes = [
     {
         titulo: "qual a capital do Brasil?",
-        respostas: ["Salvador", "Bahia", "Rio de Janeiro", "Brasília"]
+        respostas: ["Salvador", "Bahia", "Rio de Janeiro", "Brasília"],
+        certa: 3
     },
     {
         titulo: "Quantas copas a França tem?",
-        respostas: ["3", "2", "1", "0"]
+        respostas: ["3", "2", "1", "0"],
+        certa: 1
     },
      {
         titulo: "qual o nome do criador do Quiz?",
-        respostas: ["Nicolas", "Lucas", "Vinícius", "Nathan"]
+        respostas: ["Nicolas", "Lucas", "Vinícius", "Nathan"],
+        certa: 0
     }
 ];
 
- 
 
-function renderizarAlternativas() {
+// const perguntas = questoes.map(questao =>  questao.titulo);
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('pagina carregou!');
+    let i = 0;
+
+    function renderizarQuestao(i) {
     const titulo = document.querySelector(".question-bubble h2");
-     titulo.textContent = questoes[0].titulo;
+    titulo.textContent = questoes[i].titulo;
 
     const spans = document.querySelectorAll('li span');
-    const resps = questoes[0].respostas;
+    const resps = questoes[i].respostas;
    
 
     resps.forEach((texto, index)=> {
             spans[index].textContent = texto;
         });
+    }
 
-}
-renderizarAlternativas();   
+    renderizarQuestao(i);
 
+    const opcoes = document.querySelectorAll('.answer-option');
+
+    opcoes.forEach((opcao, index) => {
+        opcao.addEventListener("click", () => {
+            if (index === questoes[i].certa) {
+                 alert('acertou');
+                 i++;
+                 renderizarQuestao(i);
+            } else {
+                alert('errou'); 
+            }
+        });
+    });
+
+
+ });
