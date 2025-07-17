50a3ad684c5174839fc3b98b232d464db2849ea6
NicolasGMACK <nicolasgmack@gmail.com>
2025-07-17 11:51:29 -0300

    fix: quadro de estatísticas funcional

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const questoes = [
     {
         titulo: "qual a capital do Brasil?",
-        respostas: ["Salvador", "Bahia", "Rio de Janeiro", "Brasília"],
+        alternativas: ["Salvador", "Bahia", "Rio de Janeiro", "Brasília"],
         certa: 3
     },
     {
         titulo: "Quantas copas a França tem?",
-        respostas: ["3", "2", "1", "0"],
+        alternativas: ["3", "2", "1", "0"],
         certa: 1
     },
      {
         titulo: "qual o nome do criador do Quiz?",
-        respostas: ["Nicolas", "Lucas", "Vinícius", "Nathan"],
+        alternativas: ["Nicolas", "Lucas", "Vinícius", "Nathan"],
         certa: 0
     }
 ];
@@ -21,35 +21,57 @@ const questoes = [
 document.addEventListener("DOMContentLoaded", () => {
     console.log('pagina carregou!');
     let i = 0;
+    let numeroQuest = 1;
+    let acertos = 0;
+    let erros = 0;
 
     function renderizarQuestao(i) {
     const titulo = document.querySelector(".question-bubble h2");
     titulo.textContent = questoes[i].titulo;
 
     const spans = document.querySelectorAll('li span');
-    const resps = questoes[i].respostas;
+    const alternativas = questoes[i].alternativas;
    
 
-    resps.forEach((texto, index)=> {
+    alternativas.forEach((texto, index)=> {
             spans[index].textContent = texto;
-        });
+        }); 
     }
 
     renderizarQuestao(i);
 
+    function renderizarEstatisticas() {
+     const QuestaoElement = document.getElementById('questao');
+     const acertosElement = document.getElementById('acerto');
+     const errosElement = document.getElementById('erro');
+     QuestaoElement.textContent = numeroQuest + '/' + questoes.length;
+     acertosElement.textContent = acertos;
+     errosElement.textContent = erros;
+     };
+
+     renderizarEstatisticas();
+     
     const opcoes = document.querySelectorAll('.answer-option');
 
     opcoes.forEach((opcao, index) => {
         opcao.addEventListener("click", () => {
             if (index === questoes[i].certa) {
                  alert('acertou');
-                 i++;
-                 renderizarQuestao(i);
+                 acertos++;                 
+                 //renderizarEstatisticas();
             } else {
-                alert('errou'); 
+                alert('errou');
+                erros++;
+                 
             }
+            i++;
+            numeroQuest++;
+            renderizarQuestao(i);
+            renderizarEstatisticas();
+            ;
         });
     });
+
 
 
  });
