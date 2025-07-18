55e6d8c302e7457dce145c46b5a23f4159b28279
NicolasGMACK <nicolasgmack@gmail.com>
2025-07-17 21:16:40 -0300

    feat(executarAjuda): criando funcao que dá vida ao botão de ajuda

--- script.js
+++ script.js
@@ -59,7 +59,9 @@ document.addEventListener("DOMContentLoaded", () => {
     let numeroQuest = 1;
     let acertos = 0;
     let erros = 0;
+    
     let contadorAjuda = 3;
+    
 
     function renderizarDashboard() {
      const QuestaoElement = document.getElementById('questao');
@@ -96,17 +98,50 @@ document.addEventListener("DOMContentLoaded", () => {
    
     renderizarTela(i);
 
-    
-function executarAjuda() {
+    /*
+    Precisa fazer com que executar ajuda retire as questoes que são erradas, preservando sempre a certa.
+    Precisa fazer com que na próxima tela o icone de cadeado seja removido!.
+    Precisa ver se tem como escolher aleatoriamente entre as erradas, se for complexo faça o simples.
+    importante é resolver usando o que você sabe. Perfeito significa não feito.
+    */
+function executarAjuda(alternativaCerta) {
     const buttonHelp = document.querySelector('.help-button');
-    buttonHelp.addEventListener("click", ()=> {
+    buttonHelp.addEventListener("click", () => {
         
-    })
+        if (contadorAjuda != 0){
+            contadorAjuda--;
+        renderizarDashboard();
+        
+        let possiblidades = [0, 1, 2, 3];
+     const novoarray =   possiblidades.filter((valor) => {
+            return valor !=  alternativaCerta
+        });
+        console.log(novoarray);
+       let indiceAleatorio = Math.floor(Math.random() * novoarray.length);
+       valorReal = novoarray[indiceAleatorio];
+       const novo = novoarray.splice(indiceAleatorio, 1);
+       console.log(novo);
+
+       console.log(valorReal);
+            
+            opcoes[valorReal].classList.add('locked');
+
+            const icone = document.createElement('i');
+            icone.className = 'fas fa-lock lock-icon';
+            opcoes[valorReal].appendChild(icone);
+        
+        
+} if (contadorAjuda < 1) {
+    buttonHelp.classList.add('locked');
 }
+});
+}
+
+executarAjuda(3);
 
     function limpaClasses(){
         opcoes.forEach(conteudo => {
-        conteudo.classList.remove('certa', 'errada');
+        conteudo.classList.remove('certa', 'errada', 'locked');
         });
     };
 
@@ -115,8 +150,8 @@ function executarAjuda() {
 
     opcoes.forEach((opcao, index) => {
         opcao.addEventListener("click", () => {
-            alternativaCerta = questoes[i].certa;
             if (i < questoes.length - 1){
+            let alternativaCerta = questoes[i].certa;
                 if (index === alternativaCerta) {
                     opcoes[index].classList.add('certa');
                     acertos++;
